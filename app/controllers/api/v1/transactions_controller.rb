86eb3fa89748fb4d8263ae723b5fc95f5b92b72e
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-05 07:33:12 +0500

    Transaction create API

---
+++ app/controllers/api/v1/transactions_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class TransactionsController < ApplicationController
+
+      def create
+        @transaction = Transaction.new(transaction_params)
+        @transaction.group = @transaction.category.kind
+
+        if @transaction.category.total_amounts.find_by(member_id: @transaction.member_id).nil?
+          @transaction.category.total_amounts.create(member_id: @transaction.member_id, amount: @transaction.amount, kind: @transaction.category.kind)
+        else
+          total_amount = @transaction.category.total_amounts.find_by(member_id: @transaction.member_id).amount + @transaction.amount
+          @transaction.category.total_amounts.find_by(member_id: @transaction.member_id).update(amount: total_amount)
+        end
+
+        if @transaction.save
+          render json: @transaction, serializer: TransactionSerializer, status: :created
+        else
+          render json: { errors: @transaction.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def transaction_params
+        params.require(:transaction).permit(:name, :group, :amount, :member_id, :category_id, :created_at)
+      end
+    end    
+  end
+end
